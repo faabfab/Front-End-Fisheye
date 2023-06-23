e6b95bab54bd7f892c9ac6a2596486129201ead1
faabfab <faab.fab@free.fr>
2023-06-23 20:29:57 +0200

    fetch ok

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,22 @@
     async function getPhotographers() {
+
+        let url = "./data/photographers.json"
+        
+        let response = await fetch(url);
+
+        if (response.ok) { // if HTTP-status is 200-299
+        // obtenir le corps de réponse (la méthode expliquée ci-dessous)
+            //console.log ("c'est ok")
+            let json = await response.json()
+            let photographers = json.photographers
+            console.log(photographers)
+            return({photographers : [...photographers]})
+        } else {
+            console.log("HTTP-Error: " + response.status);
+            alert("HTTP-Error: " + response.status);
+            
+        }
+
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
@@ -40,7 +58,12 @@
          * A chaque photographe on crée une template, on crée l'affichage
          * et on l'ajoute dans .photographer_section
          */
+        //console.log(photographers)
+        
+
         photographers.forEach((photographer) => {
+
+            console.log(photographer.name)
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
