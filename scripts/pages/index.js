783d91ae29ac0aaeca55278b696aceadfb889532
faabfab <faab.fab@free.fr>
2023-06-26 11:38:36 +0200

    sass config and css begin

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,51 +1,26 @@
+    /**
+     * Fonction qui récupère les données du fichiers photographers.json
+     * @returns Tableaux des photographes
+     * return a Promise fulfilled (tenue) : l'opération a réussi
+     */
     async function getPhotographers() {
 
+        // adresse à récupérer
         let url = "./data/photographers.json"
         
+        // affectation de la promise
         let response = await fetch(url);
 
         if (response.ok) { // if HTTP-status is 200-299
         // obtenir le corps de réponse (la méthode expliquée ci-dessous)
-            //console.log ("c'est ok")
+            // récupération des données au format JSON
             let json = await response.json()
             let photographers = json.photographers
-            console.log(photographers)
             return({photographers : [...photographers]})
-        } else {
-            console.log("HTTP-Error: " + response.status);
+        } else { // Problème de récupération des données
             alert("HTTP-Error: " + response.status);
-            
         }
-
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
     }
-
-    // console.log(getPhotographers())
-    // return a Promise fulfilled (tenue) : l'opération a réussi
 
     /**
      * Affiche les données contenues dans photographers dans un format contenu dans photographerTemplate
@@ -57,13 +32,9 @@
         /**
          * A chaque photographe on crée une template, on crée l'affichage
          * et on l'ajoute dans .photographer_section
-         */
-        //console.log(photographers)
-        
+         */        
 
         photographers.forEach((photographer) => {
-
-            console.log(photographer.name)
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,7 +4,7 @@
  * @returns Les données du photographe et l'element DOM qui affiche les données
  */
 function photographerTemplate(data) {
-    const { name, portrait, tagline } = data;
+    const { name, city, country, price, portrait, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -14,16 +14,29 @@ function photographerTemplate(data) {
      */
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const a = document.createElement('a')
+        a.setAttribute('href','#')
+        const pict = document.createElement('picture')
+        pict.setAttribute('class', 'imgLayer')
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3')
+        h3.textContent = city+', '+country;
         const p =document.createElement('p')
         p.textContent = tagline
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(p)
+        const p2 =document.createElement('p')
+        p2.setAttribute('class','price')
+        p2.textContent = price+'€/jour'
+        article.appendChild(a);
+        a.appendChild(pict);
+        pict.appendChild(img);
+        a.appendChild(h2);
+        a.appendChild(h3);
+        a.appendChild(p)
+        a.appendChild(p2)
         return (article);
     }
-    return { name, picture, tagline, getUserCardDOM }
+    return { name, city, picture, tagline, getUserCardDOM }
 }
